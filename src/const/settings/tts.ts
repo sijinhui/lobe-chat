4b0380345f486bfaae81d724db5bf62863505e12
sijinhui <62741773+sijinhui@users.noreply.github.com>
2025-03-27 17:09:14 +0800

    尝试增加豆包语音支持,补充类型校验

--- src/const/settings/tts.ts
+++ src/const/settings/tts.ts
@@ -1,6 +1,9 @@
 import { UserTTSConfig } from '@/types/user/settings';
 
 export const DEFAULT_TTS_CONFIG: UserTTSConfig = {
+  doubao: {
+    ttsModel: 'zh_female_wanwanxiaohe_moon_bigtts',
+  },
   openAI: {
     sttModel: 'whisper-1',
     ttsModel: 'tts-1',
--- src/store/user/slices/settings/selectors/settings.test.ts
+++ src/store/user/slices/settings/selectors/settings.test.ts
@@ -33,6 +33,9 @@ describe('settingsSelectors', () => {
             },
           },
           tts: {
+            doubao: {
+              ttsModel: 'zh_female_wanwanxiaohe_moon_bigtts',
+            },
             openAI: {
               sttModel: 'whisper-1',
               ttsModel: 'tts-1',
