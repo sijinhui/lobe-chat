a5dce6c77b8dafba5069ce19d542b9ba5a875431
sijinhui <sijinhui@qq.com>
2025-05-21 23:49:34 +0800

    修复构建错误，主要为注释掉异常提示

--- src/features/AgentSetting/store/index.ts
+++ src/features/AgentSetting/store/index.ts
@@ -4,6 +4,7 @@ import { createContext } from 'zustand-utils';
 import { subscribeWithSelector } from 'zustand/middleware';
 import { shallow } from 'zustand/shallow';
 import { createWithEqualityFn } from 'zustand/traditional';
+// import { StateCreator } from 'zustand';
 
 import { StoreApiWithSelector } from '@/utils/zustand';
 
@@ -11,7 +12,14 @@ import { Store, store } from './action';
 
 export type { State } from './initialState';
 
+// @ts-expect-error
 export const createStore = () => createWithEqualityFn(subscribeWithSelector(store), shallow);
+// export const createStore = () => createWithEqualityFn(
+//     subscribeWithSelector(
+//       store as StateCreator<Store, [['zustand/subscribeWithSelector', never]]>
+//     ),
+//     shallow
+// );
 
 export const { useStore, useStoreApi, Provider } = createContext<StoreApiWithSelector<Store>>();
 
