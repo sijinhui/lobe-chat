994d6e708905d182868acc048de7d0fbff60fc4d
sijinhui <sijinhui@qq.com>
2025-05-22 08:35:11 +0800

    优化修复

--- src/features/AgentSetting/store/index.ts
+++ src/features/AgentSetting/store/index.ts
@@ -4,7 +4,7 @@ import { createContext } from 'zustand-utils';
 import { subscribeWithSelector } from 'zustand/middleware';
 import { shallow } from 'zustand/shallow';
 import { createWithEqualityFn } from 'zustand/traditional';
-// import { StateCreator } from 'zustand';
+import type { StateCreator } from 'zustand';
 
 import { StoreApiWithSelector } from '@/utils/zustand';
 
@@ -12,14 +12,12 @@ import { Store, store } from './action';
 
 export type { State } from './initialState';
 
-// @ts-expect-error
-export const createStore = () => createWithEqualityFn(subscribeWithSelector(store), shallow);
-// export const createStore = () => createWithEqualityFn(
-//     subscribeWithSelector(
-//       store as StateCreator<Store, [['zustand/subscribeWithSelector', never]]>
-//     ),
-//     shallow
-// );
+export const createStore = () => createWithEqualityFn(
+    subscribeWithSelector(
+      store as StateCreator<Store>
+    ),
+    shallow
+);
 
 export const { useStore, useStoreApi, Provider } = createContext<StoreApiWithSelector<Store>>();
 
