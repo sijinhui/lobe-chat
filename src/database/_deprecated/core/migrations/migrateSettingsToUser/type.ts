7012f6110e7ca7fa54915c087cc3821ccacb7c8c
sijinhui <62741773+sijinhui@users.noreply.github.com>
2025-09-15 15:14:40 +0800

    临时修复打包问题

--- src/database/_deprecated/core/migrations/migrateSettingsToUser/type.ts
+++ src/database/_deprecated/core/migrations/migrateSettingsToUser/type.ts
@@ -69,9 +69,9 @@ interface V4LLMConfig {
 }
 
 interface TTSConfig {
-  doubao: {
-    ttsModel: 'zh_female_wanwanxiaohe_moon_bigtts';
-  };
+  // doubao: {
+  //   ttsModel: 'zh_female_wanwanxiaohe_moon_bigtts';
+  // };
   openAI: {
     sttModel: 'whisper-1';
     ttsModel: 'tts-1' | 'tts-1-hd';
