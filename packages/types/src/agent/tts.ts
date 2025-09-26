0b84743d064a28a38c7973f99eb1ec10e98781f9
sijinhui <62741773+sijinhui@users.noreply.github.com>
2025-09-26 10:09:33 +0800

    fix merge

--- packages/types/src/agent/tts.ts
+++ packages/types/src/agent/tts.ts
@@ -1,10 +1,11 @@
-export type TTSServer = 'openai' | 'edge' | 'microsoft';
+export type TTSServer = 'openai' | 'edge' | 'microsoft' | 'doubao';
 
 export interface LobeAgentTTSConfig {
   showAllLocaleVoice?: boolean;
   sttLocale: 'auto' | string;
   ttsService: TTSServer;
   voice: {
+    doubao?: string;
     edge?: string;
     microsoft?: string;
     openai: string;
