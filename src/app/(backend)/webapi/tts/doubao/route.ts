1849d4e7d2ccdcc4b802626d5305e074c39bdb6a
sijinhui <sijinhui@qq.com>
2025-12-23 21:43:51 +0800

    修复多段语音问题

--- src/app/(backend)/webapi/tts/doubao/route.ts
+++ src/app/(backend)/webapi/tts/doubao/route.ts
@@ -45,7 +45,7 @@ export const POST = async (req: Request) => {
       cluster: "volcano_tts",
     },
     audio: {
-      encoding: "wav",
+      encoding: "mp3",
       speed_ratio: 1,
       voice_type: payload.options.model,
     },
@@ -58,12 +58,12 @@ export const POST = async (req: Request) => {
       uid: "豆包语音",
     }
   }
+
   const api_key = process.env.DOUBAO_VOICE_TTS_API_KEY ?? ""
 
   const resp = await fetch("https://openspeech.bytedance.com/api/v1/tts", {
     body: JSON.stringify(doubaoPayload),
     headers: {
-      // "Authorization": "Bearer;" + access_token,
       "Content-Type": "application/json",
       "x-api-key": api_key,
     },
@@ -71,7 +71,6 @@ export const POST = async (req: Request) => {
   })
 
   const result = await resp.json();
-  console.log(result.message);
   const base64Data = result.data.replace(/^data:audio\/\w+;base64,/, '');
   const binaryString = atob(base64Data);
   const arrayBuffer = new Uint8Array(binaryString.length);
@@ -79,12 +78,10 @@ export const POST = async (req: Request) => {
     arrayBuffer[i] = binaryString.charCodeAt(i);
   }
 
-  // 返回浏览器可直接播放的音频响应
   return new Response(arrayBuffer, {
     headers: {
-      // 根据实际音频类型调整
       "Content-Length": arrayBuffer.byteLength.toString(),
-      "Content-Type": "audio/wav"
+      "Content-Type": "audio/mpeg"
     }
   });
 }
